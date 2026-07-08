453e988ed2b6aea7aba9f4ab12a6b75818cf6516
Shaquib07 <md.shaquibjawaid01@gmail.com>
2026-07-09 00:42:29 +0530

    v1-first commit-basic project structure

---
+++ sync-order-pay/src/main/java/com/example/sync_order_pay/SyncOrderPayApplication.java
@@ -0,0 +1,15 @@
+package com.example.sync_order_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncOrderPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncOrderPayApplication.class, args);
+
+		System.out.println(":: welcome to payflow ::");
+	}
+
+}
---
+++ sync-order-pay/src/test/java/com/example/sync_order_pay/SyncOrderPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sync_order_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncOrderPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
